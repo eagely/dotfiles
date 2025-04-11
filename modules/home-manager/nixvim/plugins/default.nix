20bca83e7f209ca4271f7cdb69e284b0aa0fe1c1
Artemiy Smirnov <artemiy.smirnov@proton.me>
2025-04-11 23:00:04 +0200

    Add typst and nvim auto save

--- modules/home-manager/nixvim/plugins/default.nix
+++ modules/home-manager/nixvim/plugins/default.nix
@@ -1,10 +1,12 @@
 {
   imports = [
     ./autopairs.nix
+    ./auto-save.nix
     ./lsp.nix
     ./lualine.nix
     ./telescope.nix
     ./treesitter.nix
     ./trouble.nix
+    ./typst-vim.nix
   ];
 }
