af563615bc5d5526fcbc01a6f09751f91c6215bf
Artemiy Smirnov <artemiy.smirnov@proton.me>
2025-05-04 20:18:00 +0200

    chore: add fenix and some packages

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -5,16 +5,12 @@
     enable = true;
     userName = "Artemiy Smirnov";
     userEmail = "artemiy.smirnov@proton.me";
-    aliases = {
-      c = "commit";
-      s = "status";
-    };
     signing = {
       key = "~/.ssh/id_ed25519";
       signByDefault = true;
     };
     extraConfig = {
-      core.editor = "vim";
+      core.editor = "nvim";
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
       gpg.format = "ssh";
